19b3084b1eb947cc8249c2d3d8dc7bdadd73fe3f
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2024-11-30 00:18:44 +0530

    0.0.1

---
+++ fishidentification/app/(tabs)/demersal.tsx
@@ -0,0 +1,16 @@
+import { COLORS, images } from "@/constants";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
+
+export default function demersalScreen() {
+  const router = useRouter();
+
+  return (
+    <LinearGradient colors={[COLORS.blue, COLORS.blue]} style={{ flex: 1 }}>
+    <Text>
+        Billfish
+    </Text>
+    </LinearGradient>
+  );
+}
